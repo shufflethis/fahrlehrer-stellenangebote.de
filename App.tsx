3b5fbc3690368618e4a964a7e5add5aad26eb31d
Autoblogger <dev@famefact.de>
2026-03-03 14:24:21 +0100

    feat: add HR contact forms + GEO authority metadata (HR-Updater v1.0)

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import JobSeekers from './pages/JobSeekers';
 import Employers from './pages/Employers';
 import CareerInfo from './pages/CareerInfo';
 import Impressum from './pages/Impressum';
+import Datenschutz from './pages/Datenschutz';
+import AutorSeite from './pages/AutorSeite';
 
 const App: React.FC = () => {
   return (
@@ -17,7 +19,10 @@ const App: React.FC = () => {
           <Route path="/employer" element={<Employers />} />
           <Route path="/career" element={<CareerInfo />} />
           <Route path="/impressum" element={<Impressum />} />
-        </Routes>
+                  {/* // HR-UPDATER: v1.0 */}
+          <Route path="/datenschutz" element={<Datenschutz />} />
+          <Route path="/autor/thomas-sander" element={<AutorSeite />} />
+          </Routes>
       </Layout>
     </Router>
   );
